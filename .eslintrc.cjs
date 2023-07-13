4cc54ce7f46c657b8a6e7fcb1b0f6274bb7cd4e1
Simen A. W. Olsen <so@bjerk.io>
2023-07-14 01:41:28 +0200

    Update .eslintrc.cjs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,14 +1,8 @@
 module.exports = {
-  extends: ['@bjerk/eslint-config', 'plugin:jest/recommended'],
-  plugins: ['jest'],
-  overrides: [
-    {
-      files: 'jest.config.*',
-      rules: {
-        'import/no-default-export': 'off',
-      },
-    },
-  ],
+  extends: ['@bjerk/eslint-config'],
+  rules: {
+    'import/no-default-export': 'off',
+  },
   parserOptions: {
     project: true,
     tsconfigRootDir: __dirname,
